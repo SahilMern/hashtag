47b7e2852186fac2d5e9211498325f3432ee259c
Sahil yadav <sahil9082811@gmail.com>
2025-03-27 07:24:46 -0400

    removing sensitive data

--- database/connection.js
+++ database/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const dbUrl = "mongodb+srv://sahil9082811:sahilhashtag%40123@cluster0.ohtbslh.mongodb.net/hashtag?retryWrites=true&w=majority&appName=Cluster0"
+const dbUrl = ""
 
 const connection = async () => {
   try {
